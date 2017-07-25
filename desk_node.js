608d7551e30aac574d134e02037f92e29849dd4c
김현 <jun123101@gmail.com>
2017-07-25 17:28:00 +0900

    json형식으로 data 받아오기 추가

--- desk_node.js
+++ desk_node.js
@@ -12,3 +12,34 @@ myserver.createServer(function(req,res){//http객체로 서버를 만들고, fun
 console.log('http:PI-IP:1234');//어느 포트로 열렷는지 간단하게 표시합니다.
 
 
+app.post('/pushData', function(req, res){
+
+var chunk = '';
+
+//데이터를 가져옵니다.
+
+req.on('data', function(data){
+
+//데이터를 JSON으로 파싱합니다.
+
+chunk = JSON.parse(data);
+
+});
+
+req.on('end',function(){
+
+//파싱된 데이터를 확인합니다.
+
+console.log("name : "+chunk.name + " , phone : "+chunk.phone);
+
+});
+
+// 아래의 OK라는 내용이 안드로이드의 ReadBuffer를 통해
+
+// result String으로 바뀝니다.
+
+res.write("OK");
+
+res.end();
+
+});
