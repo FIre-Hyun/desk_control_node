f40b7a6348a3c9d044c0564381c1323bafe0a272
김현 <jun123101@gmail.com>
2017-08-21 15:28:26 +0900

    블루투스 잡고, 데이터보냄

--- desk_node.js
+++ desk_node.js
@@ -9,9 +9,7 @@ const bluetooth = require('node-bluetooth');
 // create bluetooth device instance 
 const device = new bluetooth.DeviceINQ();
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(bodyParser.json());
 
 app.use(express.static('public'));
@@ -36,19 +34,42 @@ app.post('/reservation', function(req, res){
   desk[req.body.desk_num] = !desk[req.body.desk_num];
 
   device.listPairedDevices(console.log);
+  if(desk[req.body.desk_num] == 1){
+   device
+   .on('finished',  console.log.bind(console, 'finished'))
+   .on('found', function found(address, name){
 
-  // device
-  //       .on('finished',  console.log.bind(console, 'finished'))
-  //       .on('found', function found(address, name){
-  //         console.log('Found: ' + address + ' with name ' + name);
-  //       }).inquire();
+    console.log('Found: ' + address + ' with name ' + name);
+
+  // find serial port channel
+  device.findSerialPortChannel(address, function(channel){
+    console.log('Found RFCOMM channel for serial port on %s: ', name, channel);
+
+    // make bluetooth connect to remote device
+    bluetooth.connect(address, channel, function(err, connection){
+      if(err) return console.error(err);
+
+      connection.on('data', (buffer) => {
+        console.log('received message:', buffer.toString());
+      });
+
+      connection.write(new Buffer('Hello!', 'utf-8'), () => {
+        console.log('wrote');
+      });
+    });
+
+  });
+
+}).inquire();
+ }
+
+ console.log(
+   '\ndesk[0] : ' + desk[0]
+   + '\ndesk[1] : ' + desk[1]
+   + '\ndesk[2] : ' + desk[2]
+   + '\ndesk[3] : ' + desk[3]);
 
 
-  console.log(
-     '\ndesk[0] : ' + desk[0]
-    + '\ndesk[1] : ' + desk[1]
-    + '\ndesk[2] : ' + desk[2]
-    + '\ndesk[3] : ' + desk[3]);
 
 
 });
