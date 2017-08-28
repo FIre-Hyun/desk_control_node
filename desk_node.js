bf58af3070c6502336a7b715b3cbb1d97b756ef7
김현 <jun123101@gmail.com>
2017-08-28 23:14:38 +0900

    책상 4개 -> 16개로 늘

--- desk_node.js
+++ desk_node.js
@@ -1,8 +1,8 @@
 
 var express = require('express');
 var app = express();
-var desk = new Array("c","c","c","c");
-var number = new Array("00", "00", "00", "00")
+var desk = new Array("c","c","c","c","c","c","c","c","c","c","c","c","c","c","c","c");
+var number = new Array("00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00", "00");
 var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
 
@@ -19,13 +19,35 @@ app.get('/', function (req, res) {
     desk1: desk[1],
     desk2: desk[2],
     desk3: desk[3],
+    desk4: desk[4],
+    desk5: desk[5],
+    desk6: desk[6],
+    desk7: desk[7],
+    desk8: desk[8],
+    desk9: desk[9],
+    desk10: desk[10],
+    desk11: desk[11],
+    desk12: desk[12],
+    desk13: desk[13],
+    desk14: desk[14],
+    desk15: desk[15],
     number0: number[0],
     number1: number[1],
     number2: number[2],
-    number3: number[3]
+    number3: number[3],
+    number4: number[4],
+    number5: number[5],
+    number6: number[6],
+    number7: number[7],
+    number8: number[8],
+    number9: number[9],
+    number10: number[10],
+    number11: number[11],
+    number12: number[12],
+    number13: number[13],
+    number14: number[14],
+    number15: number[15],
 };
-
-
 var accountStr = JSON.stringify(accountObj);
 
   console.log(accountStr);
