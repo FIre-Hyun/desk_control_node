cb81ca6ecb3bb5c55b8f8286f8d1d83c040f24cb
김현 <jun123101@gmail.com>
2017-08-28 22:51:57 +0900

    구현완료

--- desk_node.js
+++ desk_node.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var app = express();
 var desk = new Array("c","c","c","c");
+var number = new Array("00", "00", "00", "00")
 var bodyParser = require('body-parser');
 var exec = require('child_process').exec;
 
@@ -12,16 +13,34 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Connected!\n' + 
-    desk[0] + desk[1] + desk[2] + desk[3] );
+
+  var accountObj = {
+    desk0: desk[0],
+    desk1: desk[1],
+    desk2: desk[2],
+    desk3: desk[3],
+    number0: number[0],
+    number1: number[1],
+    number2: number[2],
+    number3: number[3]
+};
+
+
+var accountStr = JSON.stringify(accountObj);
+
+  console.log(accountStr);
+
+  res.send(accountStr);
+
   console.log('Someone came here' 
-  	+ '\ndesk[0] : ' + desk[0]
-  	+ '\ndesk[1] : ' + desk[1]
-  	+ '\ndesk[2] : ' + desk[2]
-  	+ '\ndesk[3] : ' + desk[3]);
+  	+ '\ndesk[0] : ' + number[0] + '학생 ' + desk[0]
+  	+ '\ndesk[1] : ' + number[1] + '학생 ' + desk[1]
+  	+ '\ndesk[2] : ' + number[2] + '학생 ' + desk[2]
+  	+ '\ndesk[3] : ' + number[3] + '학생 ' + desk[3]);
 
 
 });
+
 
 app.post('/reservation', function(req, res){
   console.log(req.body.student_num + "번 학생" + 
@@ -29,12 +48,13 @@ app.post('/reservation', function(req, res){
     req.body.desk_kind + "눌림!");
   res.send(Number(desk[req.body.desk_num]).toString());
   desk[req.body.desk_num] = req.body.desk_kind;
+  number[req.body.desk_num] = req.body.student_num;
 
- console.log(
-   '\ndesk[0] : ' + desk[0]
-   + '\ndesk[1] : ' + desk[1]
-   + '\ndesk[2] : ' + desk[2]
-   + '\ndesk[3] : ' + desk[3]);
+  console.log('Someone came here' 
+    + '\ndesk[0] : ' + number[0] + '학생 ' + desk[0]
+    + '\ndesk[1] : ' + number[1] + '학생 ' + desk[1]
+    + '\ndesk[2] : ' + number[2] + '학생 ' + desk[2]
+    + '\ndesk[3] : ' + number[3] + '학생 ' + desk[3]);
 
  if(req.body.desk_num == 0 && req.body.desk_kind == "a"){
   exec('echo "a">/dev/rfcomm0', function(err, stdout, stderr){
@@ -62,6 +82,6 @@ app.post('/reservation', function(req, res){
 
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('app listening on port 3000!');
 });
 
